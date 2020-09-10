d5523b7fbdec5cec3da245502f61a633bafdff3b
Touseef Ahmad <touseef.ahmad.cs@gmail.com>
2020-09-10 08:00:14 +0500

    moved navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // marked.js import
 // var marked = require('marked'); // error with this import using window.marked instead
+
+import Navbar from './navbar/navbar.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -42,25 +44,12 @@ Here is a random link for **LaTeX** mathematical expressions [here](http://meta.
   render() {
     return (
       <div>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a class="navbar-brand" href="#">
-            <img
-              src="https://www.nicepng.com/png/detail/187-1870373_44191585-markdown-logo.png"
-              width="30" height="30" class="d-inline-block align-top" alt="" />
-                &nbsp;&nbsp;&nbsp; Markdown Previewer
-            </a>
-          <span class="navbar-text">
-            You'll never see a better markdown previewer
-          </span>
-        </nav>
-
+        
+        <Navbar/>
         <div className="row">
           <div className="col-md-6">
-
             <textarea className="form-control border-0" style={{ height: '700px' }} id="editor" value={this.state.markdownText} onChange={this.handleChange}></textarea>
-
           </div>
-
           <div id="preview" className="card card-body bg-light col-md-6" dangerouslySetInnerHTML={{ __html: this.state.convertedHtml }} />
         </div>
       </div>
